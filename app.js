272b517c08a6c60f1a913e33fbf36a94b3b7168a
Alejandro-Elias <alejandroelias25@gmail.com>
2023-09-18 10:35:28 -0300

    agrega estructura switch

---
+++ app.js
@@ -0,0 +1,27 @@
+// hacer el front de la calculadora
+
+const process = require("process");
+
+const {argv} = process;
+
+function calculadora(comando) {
+    switch (comando) {
+        case "sumar":
+            
+            break;
+        case "restar":
+            
+            break;
+        case "mulltiplicar":
+            
+            break;
+        case "dividir":
+            
+            break;
+    
+        default:
+            return "comando no valido"
+            break;
+    }
+}
+
