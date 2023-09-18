9a0b2bdb9cd5032d4564ea695f59d3979a4a2fc7
Alejandro-Elias <alejandroelias25@gmail.com>
2023-09-18 16:45:02 -0300

    agragar sumar

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // hacer el front de la calculadora
 
-const sumar = require("sumar")
+const sumar = require("./sumar")
 
 const process = require("process");
 
 const {argv} = process;
 
-function calculadora(comando) {
+function calculadora(comando, a, b) {
     switch (comando) {
         case "sumar":
            return sumar(a,b)                        
@@ -23,7 +23,7 @@ function calculadora(comando) {
     
         default:
             return "comando no valido"
-            break;
     }
 }
 
+console.log(calculadora(argv[2], +argv[3], +argv[4]));
